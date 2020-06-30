e3fd4d7e9e87f6b4aaae50e891306f8d41dbea0e
Zavrazhnov Pavel <pavel-galant@mail.ru>
2020-06-30 07:56:02 +0300

    03 section ready

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,17 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {return '';}
+  if (size === undefined) {return string;}
 
+  return string.split('').reduce((last, item) => {
+    if (last.lastChar !== item) { // смена символа
+      return {lastChar: item, count: 1, result: last.result + item};
+    }
+
+    return {
+      lastChar: item,
+      count: last.count + 1,
+      result: (last.count < size) ? last.result + item : last.result};
+  }, {lastChar: '', count: 0, result: ''}).result;
 }
